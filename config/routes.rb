933dcbec6a3908dea71885480188a5a1e74fb54b
dashingshakeel <shkeelrauf@gmail.com>
2016-09-12 02:53:07 +0500

    Usercontroller generated for authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  get 'users/new'
+
   root to: 'static_page#home'
   get '/help', to: 'static_page#help'
   get '/about', to: 'static_page#about'
