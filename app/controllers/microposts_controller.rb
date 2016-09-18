57c9417798180fba906bb2f542712c595ca90d23
dashingshakeel <shkeelrauf@gmail.com>
2016-09-18 23:59:34 +0500

    Upload image added  in posts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -24,6 +24,6 @@ class MicropostsController < ApplicationController
     end
 
    def  micropost_params
-     params.require(:micropost).permit(:content)
+     params.require(:micropost).permit(:content,:picture)
    end
 end
