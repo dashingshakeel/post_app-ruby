7bf91ce0f3320563b0949e2829d2ac07049a0bdf
dashingshakeel <shkeelrauf@gmail.com>
2016-09-18 16:34:48 +0500

    Complete activation of accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User  < ActiveRecord::Base
   attr_accessor :remember_token,:forget,:activation_token
-  before_save { self.email  = email.downcase  }
+  before_save :downcase_email
   validates :name,  presence: true, length: { maximum:  50  }
   before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -24,18 +24,30 @@ class User  < ActiveRecord::Base
   end
 
     # Returns true  if  the given token matches the digest.
-  def authenticated?(remember_token)
-    return  false if  remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest  = send("#{attribute}_digest")
+    return  false if  digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  def activate
+    update_attribute(:activated,        true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  # Sends activation  email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
   def forget
     update_attribute(:remember_token,  nil)
   end
-  def downcase_email
-    self.email  = email.downcase
-  end
-  def create_activation_digest
-    self.activation_token   = User.new_token
-    self.activation_digest  = User.digest(activation_token)
-  end
+
+  private
+
+    def downcase_email
+      self.email  = email.downcase
+    end
+    def create_activation_digest
+      self.activation_token   = User.new_token
+      self.activation_digest  = User.digest(activation_token)
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  host  = 'localhost:3000'
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -19,6 +17,9 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  host  = 'localhost:25'
+  config.action_mailer.delivery_method  = :test
   config.action_mailer.default_url_options  = { host: host  }
 
   # Raise an error on page load if there are pending migrations.
