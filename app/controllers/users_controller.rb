bd06f6c1346d2dd8d7c32178e574a3c14f60023c
dashingshakeel <shkeelrauf@gmail.com>
2016-09-18 18:44:32 +0500

    Created and designed micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     @users  = User.paginate(page: params[:page])
   end
   def show
-    @user=User.find(params[:id])
+      @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
